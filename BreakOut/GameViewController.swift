eb661ea85cea86f8fbe2e46fb6db912b9fe8b61a
SESP Walkup <sespwalkup@staffmac08-sesp.local>
2016-07-28 13:42:42 -0500

    Created ball and paddle

--- BreakOut/GameViewController.swift
+++ BreakOut/GameViewController.swift
@@ -14,11 +14,25 @@ class GameViewController: UIViewController {
     var collisionBehavior = UICollisionBehavior()
     var paddle = UIView()
     var ball = UIView()
-    var brick = UIView()
     var lives = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Ball
+        ball = UIView(frame: CGRectMake(view.center.x, view.center.y, 20, 20))
+        ball.backgroundColor = UIColor.brownColor()
+        ball.layer.cornerRadius = 10
+        ball.clipsToBounds = true
+        view.addSubview(ball)
+        
+        // Paddle
+        paddle = UIView(frame: CGRectMake(view.center.x, view.center.y * 1.7, 80, 20))
+        paddle.backgroundColor = UIColor.whiteColor()
+        view.addSubview(paddle)
+        
+        
+        
     }
     
     @IBAction func dragPaddle(sender: UIPanGestureRecognizer) {
